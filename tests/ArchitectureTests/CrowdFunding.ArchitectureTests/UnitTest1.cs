015084bf45101d3bac4f6b2edc312dd2ef4d2bc6
Maysam Gamini <maysam.gamini@aristocrat.com>
2026-04-01 15:28:15 -0500

    01-init-monolitic-project-structure

---
+++ tests/ArchitectureTests/CrowdFunding.ArchitectureTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace CrowdFunding.ArchitectureTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/IntegrationTests/CrowdFunding.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace CrowdFunding.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
