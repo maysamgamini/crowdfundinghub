1438bd85fcb285970b7876058ca6a57bce812759
Maysam Gamini <maysam.gamini@aristocrat.com>
2026-04-06 23:26:26 -0500

    Remove obsolete cross-module gateway services

--- src/Modules/Campaigns/CrowdFunding.Modules.Campaigns.Application/Abstractions/Services/ICampaignModerationGateway.cs
+++
@@ -1,7 +0,0 @@
-namespace CrowdFunding.Modules.Campaigns.Application.Abstractions.Services;
-
-public interface ICampaignModerationGateway
-{
-    Task CreateReviewAsync(Guid campaignId, CancellationToken cancellationToken);
-    Task EnsureApprovedForPublishingAsync(Guid campaignId, CancellationToken cancellationToken);
-}
